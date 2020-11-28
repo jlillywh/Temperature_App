afc73d7a6d1490b72c800318a218860e55464200
JasonLillywhite <jason.lillywhite@gmail.com>
2020-11-28 09:42:32 -0800

    Initial commit

---
+++ app/src/main/java/com/example/helloworldapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.helloworldapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "com.example.helloworldapp.MESSAGE"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /**
+     * Called when the user taps the Send button
+     */
+    fun sendMessage(view: View){
+        val editText = findViewById<EditText>(R.id.editText)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+}
